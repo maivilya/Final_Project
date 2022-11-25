7fd36fa9f64cc19b8d270096da2124746ba41a0e
Ilya <mamaevila2004@mail.ru>
2022-11-25 15:10:44 +0500

    ready programm

--- Final_Project/Program.cs
+++ Final_Project/Program.cs
@@ -1,1 +1,45 @@
-﻿
+﻿Console.Write("сколько элементов будет введено: ");
+int size = Convert.ToInt32(Console.ReadLine());
+string[] arrayStrings = new string[size];
+InsertUserElements(size);
+string[] resultArray = MakeResultArray(size, arrayStrings);
+PrintArray(resultArray);
+
+
+
+
+
+void InsertUserElements(int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"введите {i + 1}-й элемент: ");
+        string element = Convert.ToString(Console.ReadLine());
+        arrayStrings[i] = element;
+    }
+}
+
+string[] MakeResultArray(int size, string[] arrayStr)
+{
+    string[] arrayFinal = new string[size];
+    int length = 3;
+    int position = 0;
+    for (int i = 0; i < size; i++)
+    {
+        if (arrayStr[i].Length <= length)
+        {
+            arrayFinal[position] = arrayStr[i];
+            position++;
+        }
+    }
+    return arrayFinal;
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array.Length >= 3) Console.Write($"[{array[i]}], ");
+        else Console.Write($"[{array[i]}]");
+    }
+}
